a99e70f57d744686c03617fb188c32ff0f077c43
tuanflute275 <tuanflute275@gmail.com>
2025-01-21 19:45:03 +0700

    Author role account

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,29 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Login from "./components/login/Login";
 import { adminRoutes, clientRoutes } from "./routes/Routes";
 import { useSelector } from "react-redux";
 import { selectUserData } from "./redux/reducers/user";
 
+const ProtectedRoute = ({ isAuthenticated, children }) => {
+  return isAuthenticated ? children : <Navigate to="/login" />;
+};
+
 function App() {
   const userData = useSelector(selectUserData);
+  const isAuthenticated = userData?.id;
   return (
     <Routes>
-      <Route path="/login" element={<Login />} />
+      {adminRoutes.map((route, index) => {
+        const element = route.protected ? (
+          <ProtectedRoute isAuthenticated={isAuthenticated}>
+            {route.element}
+          </ProtectedRoute>
+        ) : (
+          route.element
+        );
 
-      {/* check role  */}
-      {/* {userData.user.role[0].roleName === "Admin" &&
-        adminRoutes.map((route, index) => {
-          return (
-            <Route
-              exact
-              key={index}
-              path={route.path}
-              element={route.element}
-            />
-          );
-        })} */}
-
-         {/* do not check role  */}
-         { adminRoutes.map((route, index) => {
-          return (
-            <Route
-              exact
-              key={index}
-              path={route.path}
-              element={route.element}
-            />
-          );
-        })}
+        return <Route exact key={index} path={route.path} element={element} />;
+      })}
         
       {clientRoutes.map((route, index) => {
         return (
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -6,6 +6,7 @@ import AdminHome from "../components/admin/pages/home/AdminHome";
 import AddProduct from "../components/admin/pages/product/add/AddProduct";
 import ListProduct from "../components/admin/pages/product/list/ListProduct";
 import UpdateProduct from "../components/admin/pages/product/update/UpdateProduct";
+import Login from "../components/login/Login";
 import UserMasterLayout from "../components/user/layouts/UserMasterLayout";
 import Detail from "../components/user/pages/detail/Detail";
 import Home from "../components/user/pages/home/Home";
@@ -13,32 +14,44 @@ import Shop from "../components/user/pages/shop/Shop";
 
 export const adminRoutes = [
   {
+    path: "/login",
+    element: <Login />,
+    protected: false
+  },
+  {
     path: "/admin",
     element: <AdminMasterLayout child={<AdminHome />} />,
+    protected: true
   },
   {
     path: "admin/category",
     element: <AdminMasterLayout child={<ListCategory />} />,
+    protected: true
   },
   {
     path: "admin/category/add",
     element: <AdminMasterLayout child={<AddCategory />} />,
+    protected: true
   },
   {
     path: "admin/category/update/:id",
     element: <AdminMasterLayout child={<UpdateCategory />} />,
+    protected: true
   },
   {
     path: "admin/product",
     element: <AdminMasterLayout child={<ListProduct />} />,
+    protected: true
   },
   {
     path: "admin/product/add",
     element: <AdminMasterLayout child={<AddProduct />} />,
+    protected: true
   },
   {
     path: "admin/product/update/:id",
     element: <AdminMasterLayout child={<UpdateProduct />} />,
+    protected: true
   }
 ];
 
